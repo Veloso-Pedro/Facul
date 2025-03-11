dc0fd6cb68f3af8c28613b9a3bce4d2357faa433
Veloso-Pedro <pevesalima@Gmail.com>
2025-03-10 23:58:23 -0300

    Receitas de café e sanduíche

---
+++ ReceitaCafe.java
@@ -0,0 +1,15 @@
+public class ReceitaCafe {
+    public static void main(String[] args) {
+        System.out.println("Início");
+        System.out.println("1° Ferver água");
+        System.out.println("2° Colocar fitro de papel em um suporte");
+        System.out.println("3° Adicionar café em pó no filtro");
+        System.out.println("4° Posicionar o suporte sobre xícar ou reciiente");
+        System.out.println("5° Despejar água quente sobre o café no filtro");
+        System.out.println("6° Dexar o café coar lentamente");
+        System.out.println("7° Remover filtro com café");
+        System.out.println("8° Descartar o filtro usado");
+        System.out.println("9° Servir o café na xícara");
+        System.out.println("Fim");
+    }
+}
---
+++ ReceitaSanduiche.java
@@ -0,0 +1,14 @@
+public class ReceitaSanduiche {
+    public static void main(String[] arg){
+        System.out.println("*Sacola de pão de forma em mãos");
+        System.out.println("*pote de presunto e queijo em mãos");
+        System.out.println("1° Abrir o saco de pão de forma");
+        System.out.println("2° Pegar duas fatias de pão");
+        System.out.println("3° Pegar o pote de presunto e queijo");
+        System.out.println("4° Abrir o pote de presunto e queijo");
+        System.out.println("5° Pegar uma fatia de presunto e colocar sobre uma fatia de pão");
+        System.out.println("6° Pegar uma fatia de queijo e colocar sobre a fatia de presunto que já está no pão");
+        System.out.println("7° Pegar a outra fatia de pão e colocar sobre a fatia do quijo");
+        System.out.println("fim!");
+    }
+}
