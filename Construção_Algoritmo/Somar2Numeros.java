b2a9191f76d6c6cb97d917cf081d30d319e89b5d
Veloso-Pedro <pevesalima@Gmail.com>
2025-03-24 21:44:51 -0300

    Create Somar2Numeros.java

---
+++ Construção_Algoritmo/Somar2Numeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Somar2Numeros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Calculadora de dois números");
+        
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("O resultado da sua soma é "+(num1+num2));
+
+        scanner.close();
+    }
+}
