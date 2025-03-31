605b4ea73676909f84f401b39b94ce93b800bd7c
Veloso-Pedro <pevesalima@Gmail.com>
2025-03-31 19:30:45 -0300

    Update Ac1Media.java

--- Construção_Algoritmo/Ac1Media.java
+++ Construção_Algoritmo/Ac1Media.java
@@ -8,6 +8,7 @@ public class Ac1Media {
         double Ac2 = 0;
         double Ag = 0;
         double Af = 0;
+        double Nm = 0;
         
         // Entrada das variáveis
         System.out.println("Calculadora de média");
@@ -20,12 +21,15 @@ public class Ac1Media {
         System.out.println("===============================");
         System.out.print("Digite sua nota da Af: "); Af = scanner.nextDouble();
         System.out.println("===============================");
+        System.out.print("Digite a nota mínima para aprovação:"); Nm = scanner.nextDouble();
+        System.out.println("==============================="); 
+
         // Calculando a média e prova real para garantir que os calculos estão certos
         double media = (Ac1 * 0.15) + (Ac2 * 0.30) + (Ag * 0.10) + (Af * 0.45);
         //System.out.println(Ac1 * 0.15); System.out.println(Ac2 * 0.30); System.out.println(Ag * 0.10); System.out.println(Af * 0.45);
 
         // Saída dos dados
-        System.out.println("Nota mínima para aprovação: 5.0");
+        System.out.println("Nota mínima para aprovação: "+ (Nm));
         System.out.println("===============================");
 
         System.out.println("Sua nota Ac1: "+ (Ac1));
@@ -37,7 +41,7 @@ public class Ac1Media {
         System.out.printf("Sua média é %.1f%n ", media);
         System.out.println("================================");
         
-        if (media >= 5){
+        if (media >= Nm){
             System.out.println("Status: Aprovado!");
             System.out.println("=================");
         }else{
