e95a7b53396576aa18b13470cdecee4a2fc0b7b8
Veloso-Pedro <pevesalima@Gmail.com>
2025-04-14 19:09:37 -0300

    Update AnoBisexto.java

--- Construção_Algoritmo/AnoBisexto.java
+++ Construção_Algoritmo/AnoBisexto.java
@@ -3,5 +3,15 @@ import java.util.Scanner;
 public class AnoBisexto {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+
+        System.out.println("Digite um número: "); 
+        int ano = scn.nextInt();
+
+        if (ano % 4 == 0 || ano % 400 == 0){
+            System.out.println("ANO BISEXTO!!");
+        }else{
+            System.out.println("ANO NORMAL!");
+        }
+        scn.close();
     }
 }
