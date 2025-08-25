1f620de0daa752b7fdf5ff391001479b4cb37265
Veloso-Pedro <pevesalima@Gmail.com>
2025-08-25 19:40:04 -0300

    Update ListaExercicio2.java

--- POO/Aula1/ListaExercicio2.java
+++ POO/Aula1/ListaExercicio2.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.ZoneId;
+
+
 
 public class ListaExercicio2 {
     
@@ -25,12 +32,68 @@ public class ListaExercicio2 {
                 "Preço de Custo: R$ " + String.format("%.2f", this.precoCusto)  + "\n" +
                 "Preço de Venda: R$ " + String.format("%.2f", this.precoVenda) + "\n"; 
         }
+
+    }
+
+    public static class Pessoa{
+        String nome;
+        String sobrenome;
+        LocalDate dataN;
+        String email;
+
+        Pessoa(String nome, String sobrenome, String dataN){
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.dataN = validarData(dataN);
+            this.email = nome.toLowerCase() + "." + sobrenome.toLowerCase() + "@dominio.";
+        }
+
+        private LocalDate validarData(String dataTexto) {
+            try {
+                // Formato dd/MM/yyyy
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                LocalDate data = LocalDate.parse(dataTexto, formatter);
+
+                // Comparador: não pode ser no futuro
+                if (data.isAfter(LocalDate.now())) {
+                    throw new IllegalArgumentException("Data de nascimento não pode ser no futuro!");
+                }
+
+                return data;
+            } catch (DateTimeParseException e) {
+                throw new IllegalArgumentException("Data inválida! Use o formato dd/MM/yyyy");
+            }
+        }
+
+        // Converter data para Epoch (segundos)
+        public long getDataNascimentoEpoch() {
+            return this.dataN.atStartOfDay(ZoneId.systemDefault()).toEpochSecond();
+        }
+
+
+        public String descricao(){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            return
+                "nome: " + this.nome + "\n" +
+                "sobrenome: " + this.sobrenome + "\n" +
+                "Data de Nascimento: " + this.dataN.format(formatter) + "\n" +
+                "Email: " + this.email + "\n";
+        }
+
+
     }
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         Produto p1 = new Produto("Camiseta", "Nike", 15.50, 65.00);
 
         System.out.println( p1.descricao());
         System.out.println("Lucro: R$ " + String.format("%.2f", p1.lucro()));
+
+        System.out.println("==========================================================");
+
+        Pessoa people = new Pessoa("Carlos", "Lima", "22/08/2025");
+
+        System.out.println(people.descricao());
     } 
 }
