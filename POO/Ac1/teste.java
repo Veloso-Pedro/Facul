5cb7a435796ba83d2edd380df983808b7e791cd0
Pedro Veloso <pevesalima@Gmail.com>
2025-09-23 12:30:34 -0300

    23/09

--- POO/Ac1/teste.java
+++ POO/Ac1/teste.java
@@ -5,25 +5,25 @@ public class teste {
 
         System.out.println("===== Veículo =====");
         //*Modelo,montadora,placa,ano,cor,valor 
-        Veiculos v = new Veiculos("Focus", "Ford", "FoP6i74", "2010", "Azul", 25000.00);
+        Veiculos v = new Veiculos("Focus", "Ford", "FoP6i74", "2010", "Branco", 25000.00);
 
         System.out.println(v.descricaoVeiculo());
 
         System.out.println("===== Carro =====");
         //*Modelo,montadora,placa,ano,cor,valor,qtdportas,potencia,cambio,freiosabs,kitcamera
-        Carros c = new Carros("Focus", "Ford", "fop6874",  "2010", "Vermelho", 25000.00, 4, 250, "Automatico");
+        Carros c = new Carros("Focus", "Ford", "fop6874",  "2010", "Preto", 25000.00, 4, 250, "Automatico");
 
         System.out.println(c.descricaoCarros());
 
         System.out.println("===== Moto =====");
         //*Modelo,montadora,placa,ano,cor,valor,cilindrada,protetormotor,alarme,bau
-        Motos m = new Motos("R 1250 GS", "bwm", "lub4990", "2010", "Vermelho", 25000.00, 1254);
+        Motos m = new Motos("R 1250 GS", "bwm", "lub4990", "2010", "Prata", 25000.00, 1254);
 
         System.out.println(m.descricaoMotos());
 
         System.out.println("===== Caminhão =====");
         //*Modelo,montadora,placa,ano,cor,valor,qqtdeixos,cargamax,faixarefletiva,luzlateral,lona
-        Caminhoes i = new Caminhoes("Actros Evolution 2653 S 6x4", "mercedes", "NEX-4811", "2010", "Vermelho", 25000.00, 9, 74);
+        Caminhoes i = new Caminhoes("Actros Evolution 2653 S 6x4", "mercedes", "NEX-4811", "2010", "Branco", 25000.00, 9, 74);
 
         System.out.println(i.descricaoCaminhoes());
     }
